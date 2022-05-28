3d837ff64bb76bb2070558b8ab5e70a07e2f6e5d
kaisic1224 <89894485+kaisic1224@users.noreply.github.com>
2022-05-28 11:51:19 -0400

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title></title>
+      </Head>
+    </>
+  );
+};
+
+export default Home;
