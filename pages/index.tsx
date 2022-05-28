b7f78a48d3943b61aa9e126e3431aa3565ead5c4
Christopher Fong <79167402+christopherfong115@users.noreply.github.com>
2022-05-28 13:06:48 -0400

    Add files via upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const Home: NextPage = () => {
+  const { data: session } = useSession();
   return (
     <>
       <Head>
         <title></title>
       </Head>
+      <div className="bg-red-700 text-white">Hello world</div>
     </>
   );
 };
---
+++ pages/signin.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const signin = () => {
+  return <div>signing</div>;
+};
+
+export default signin;
