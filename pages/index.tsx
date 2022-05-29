c8a7a8127e919600349d2b7ac20faaae1c574e17
Christopher Fong <79167402+christopherfong115@users.noreply.github.com>
2022-05-28 20:26:25 -0400

    Add files via upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,25 +3,44 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { useSession, signIn, signOut } from "next-auth/react";
-
 import ChatBubble from "../components/ChatBubble";
 import SendMessage from "../components/SendMessage";
 import ChatWindow from "../components/ChatWindow";
 
-import NavBar from "../components/NavBar";
-
-
 const Home: NextPage = () => {
   const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        <Head>
+          <title>Recycle Master</title>
+        </Head>
+        <h1 className="text-4xl font-bold text-blue-700">
+          Signed in as {session?.user?.name}
+        </h1>
+        <button
+          className="p-4 bg-green-400 rounded-2xl mt-4"
+          onClick={() => signOut()}
+        >
+          Sign Out
+        </button>
+        <ChatWindow />
+      </>
+    );
+  }
   return (
     <>
       <Head>
-        <title></title>
+        <title>Recycle Master</title>
       </Head>
-      <NavBar />
+      <h1 className="text-4xl font-bold">Not signed in.</h1>
+      <button
+        className="p-4 bg-amber-200 rounded-2xl mt-4"
+        onClick={() => signIn()}
+      >
+        Sign In
+      </button>
       <ChatWindow />
-
- 
     </>
   );
 };
